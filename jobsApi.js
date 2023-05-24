c3aeab45bb1471f6b1499c1279c34a5b22bed41b
Ali Elmansoury <73143531+Ali-Elmansoury@users.noreply.github.com>
2023-05-24 20:00:29 +0300

    Api is working aaaayyyyyyyyyyyyyyyyyyyyyyy

--- jobsApi.js
+++ jobsApi.js
@@ -1,23 +1,16 @@
-/*var myhttp = new XMLHttpRequest();
-myhttp.open('GET', 'job_data.json');
-myhttp.send();
-myhttp.addEventListener("readystatechange", function () {
-    if (myhttp.readyState == 4 && myhttp.status == 200) {
-        var data = JSON.parse(myhttp.response);
-        console.log(data)
-    }
-})*/
+var btngan = document.getElementById('va');
+btngan.onclick = getJobs();
 
- async function getJobs() {
+async function getJobs() {
     // Make a GET request to the API endpoint
-    fetch('https://api.adzuna.com/v1/api/jobs/us/search/1?app_id=cc3c6bd4&app_key=3fe3e17eff25c1091b93ff10a8f6a8f3&results_per_page=20')
+    fetch('job_data.json')
         .then(response => response.json())
         .then(data => {
-            /*// Extract the information from each job
-            const jobs = data.results;
+            // Extract the information from each job
+            const jobs = data;
 
             // Get the container element in which to replace the job data
-            const container = document.getElementById('jobsContainer');
+            const container = document.getElementById('jobs-Container');
 
             // Remove existing job elements
             while (container.firstChild) {
@@ -27,25 +20,26 @@ myhttp.addEventListener("readystatechange", function () {
             // Loop through the jobs and create HTML elements for each job
             for (const job of jobs) {
                 // Extract the job information
-                const categoryLabel = job.category.label;
-                const companyDisplayName = job.company.display_name;
-                const companyCreated = job.created;
-                const companyDescription = job.description;
-                const locationArea = job.location.area;
-                const salaryMax = job.salary_max;
+                const categoryLabel = job.Category;
+                const companyDisplayName = job['Company Display Name'];
+                const companyCreated = job['Company Created'];
+                const companyDescription = job['Company Description'];
+                const locationArea = job['Location Area'];
+                const salaryMax = job['Salary Max'];
+                const title = job['Title'];
 
                 // Create HTML elements for the job data
                 const jobElement = document.createElement('div');
                 jobElement.classList.add('box');
                 jobElement.innerHTML = `
                 <div class="company">
-                  <img src="imgs/icon-1.png" alt="">
+                  <img src="imgs/logo-placeholder.jpg" alt="">
                   <div>
                     <h3>${companyDisplayName}</h3>
                     <p>${companyCreated}</p>
                   </div>
                 </div>
-                <h3 class="job-title">${categoryLabel}</h3>
+                <h3 class="job-title">${title}</h3>
                 <p class="location"><i class="fas fa-map-market-alt"></i> <span>${locationArea}</span></p>
                 <div class="tags">
                   <p><i class="fas fa-indian-rupee-sign"></i><span>${salaryMax}</span></p>
@@ -60,10 +54,44 @@ myhttp.addEventListener("readystatechange", function () {
 
                 // Append the job element to the container
                 container.appendChild(jobElement);
-                console.log(data)
-            }*/
-            console.log(data)
-        })
-        
+            }
 
+        })
+
+        .catch(error => {
+            // Handle error if the JSON file cannot be fetched
+            console.error('Error:', error);
+        });
+
+}
+
+var btngana = document.getElementById('searchJob');
+btngana.onclick = searchJobs();
+
+var jobTitle = document.getElementById('jtitle');
+var jobLocation = document.getElementById('jloc');
+
+async function searchJobs() {
+    const url = 'https://linkedin-jobs-search.p.rapidapi.com/';
+    const options = {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json',
+            'X-RapidAPI-Key': '1e17914584msh439b3ecd9b04e01p17ae16jsn15db0eb1f70e',
+            'X-RapidAPI-Host': 'linkedin-jobs-search.p.rapidapi.com'
+        },
+        body: {
+            search_terms: jobTitle.value,
+            location: jobLocation.value,
+            page: '1'
+        }
+    };
+
+    try {
+        const response = await fetch(url, options);
+        const result = await response.text();
+        console.log(result);
+    } catch (error) {
+        console.error(error);
+    }
 }
